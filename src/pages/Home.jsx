c7dbc68063a06ebf91c8fa5cc9abe9bf7f65cf8d
Guransh Dhaliwal <guranshcodes@outlook.com>
2026-07-07 16:57:25 -0400

    dsa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,10 @@ import SkillsSection from '@/components/portfolio/SkillsSection';
 import GoalsSection from '@/components/portfolio/GoalsSection';
 import ActivitiesSection from '@/components/portfolio/ActivitiesSection';
 import ContactFooter from '@/components/portfolio/ContactFooter';
-import aboutImage from '../../177470905.png';
-
 const baseUrl = import.meta.env.BASE_URL || '/';
 const HERO_IMAGE = `${baseUrl}images/unnamed.png`;
 const PROJECTS_IMAGE = `${baseUrl}images/projects-illustration.svg`;
-const ABOUT_IMAGE = aboutImage;
+const ABOUT_IMAGE = `${baseUrl}images/hero-illustration.svg`;
 
 export default function Home() {
   return (
